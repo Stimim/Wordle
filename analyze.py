09711b742d479fd3406880aa01968da8bbfadcbd
Stimim Chen <stimim@google.com>
2022-01-25 14:50:33 +0800

    analyze.py: show recommended guess

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
+import algo1
 import argparse
-import typing
 
 import common
 
@@ -18,12 +18,17 @@ def Main():
   answer = args.answer
   answer_id = guess_to_id[answer]
 
+  player = algo1.Player(database)
+  player.Init()
+
   possibilities = set(range(database.answer_count))
   for try_count in range(6):
     if try_count > 0:
       word_list = [database.guesses[i] for i in possibilities]
       print(f'{len(possibilities)} left: {word_list}')
 
+    player_guess = player.MakeAGuess()
+    print(f'{database.guesses[player_guess]}')
     guess = input(f'guess {try_count + 1}: ')
     if guess not in guess_to_id:
       continue
@@ -36,6 +41,7 @@ def Main():
         break
     assert result != -1
 
+    player.UpdateResult(guess_id, result)
     if guess_id == answer_id:
       print(f'Done')
       break
